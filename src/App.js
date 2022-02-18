33cdd73884507e9b4cf0f77b6a2c59cd05e5feb8
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-02-18 16:38:26 +0100

    Zaimplementowano zabezpieczenie przed dodawaniem istniejących już kontaktów

--- src/App.js
+++ src/App.js
@@ -12,12 +12,17 @@ class App extends Component {
 
   addContact = (name, number) => {
     const contact = { name: name, number: number };
+    const foundContant = this.state.contacts.find(contact => contact.name === name);
+    if (foundContant) {
+      alert(name + ' is already in contacts');
+      return;
+    }
     this.setState(({ contacts }) => ({ contacts: [contact, ...contacts], name: '', number: '' }));
   };
 
   handleFilter = event => {
     const value = event.target.value;
-    this.setState({[event.target.name]: value });
+    this.setState({ [event.target.name]: value });
   };
 
   render() {
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -8,6 +8,7 @@ function ContactList({ list, filter }) {
         {contact.name}:{contact.number}
       </li>
     ));
+  
 
   return <ul>{contacts}</ul>;
 }
