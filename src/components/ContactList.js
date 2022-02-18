d632e08518adf4a9d3330a79c641fcc05d19c178
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-02-18 13:33:44 +0100

    Dodano możliwość filtrowania kontaktów

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,11 +1,13 @@
 import { nanoid } from 'nanoid';
 
 function ContactList({ list, filter }) {
-  let contacts = list.map(contact => (
-    <li key={nanoid()}>
-      {contact.name}:{contact.number}
-    </li>
-  ));
+  let contacts = list
+    .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+    .map(contact => (
+      <li key={nanoid()}>
+        {contact.name}:{contact.number}
+      </li>
+    ));
 
   return <ul>{contacts}</ul>;
 }
